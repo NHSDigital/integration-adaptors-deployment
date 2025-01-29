08034291dd8cf44c83da288f90a2703fa7a26cc3
Adrian Clay <adrian.clay1@nhs.net>
2025-01-29 14:47:52 +0000

    Move AWS provider constraint to required_providers block
    Fixes warning:
    > Warning: Version constraints inside provider configuration blocks are deprecated

--- aws/components/nhais/terraform.tf
+++ aws/components/nhais/terraform.tf
@@ -3,10 +3,16 @@ terraform {
   backend "s3" {
     # Intentionally blank - all parameters provided in command line
   }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.37.0"
+    }
+  }
 }
 
 # Setup AWS provider
 provider "aws" {
-  version = "~> 3.28"
   region = var.region
 }
