e1e3f6582aac0ee84792f8c9669f3087ff70832d
Chiara <chiara.mapelli@madetech.com>
2025-08-07 16:18:55 +0100

    Adding last secrets

--- aws/components/OneOneOne/data_secrets.tf
+++ aws/components/OneOneOne/data_secrets.tf
@@ -10,6 +10,15 @@ data "aws_secretsmanager_secret" "pem111_queue" {
   name = "PEM111_AMQP_QUEUE_NAME"
 }
 
+data "aws_secretsmanager_secret" "pem111_itk_ods_code_list" {
+  name = "PEM111_ITK_ODS_CODE_LIST"
+}
+
+data "aws_secretsmanager_secret" "pem111_itk_external_configuration_url" {
+  name = "PEM111_ITK_EXTERNAL_CONFIGURATION_URL"
+}
+
+
 # nginx secrets
 
 data "aws_secretsmanager_secret" "nginx_server_certificate" {
--- aws/components/OneOneOne/locals_env_variables.tf
+++ aws/components/OneOneOne/locals_env_variables.tf
@@ -5,20 +5,25 @@ locals {
       value = replace(data.aws_mq_broker.OneOneOne_mq_broker.instances[0].endpoints[1],"amqp+ssl","amqps") # https://www.terraform.io/docs/providers/aws/r/mq_broker.html#attributes-reference
     },
     {
-      name = "PEM111_AMQP_QUEUE_NAME"
-      value = data.aws_secretsmanager_secret.pem111_queue.name
-    },
-    {
       name = "LOG_LEVEL"
       value = var.OneOneOne_log_level
+    }
+  ])
+}
+
+locals {
+  secret_variables = [
+    {
+      name      = "PEM111_AMQP_QUEUE_NAME"
+      valueFrom = data.aws_secretsmanager_secret.pem111_queue.arn
     },
     {
       name = "PEM111_ITK_ODS_CODE_LIST"
-      value = "EM396"
+      valueFrom = data.aws_secretsmanager_secret.pem111_itk_ods_code_list.arn
     },
     {
       name = "PEM111_ITK_EXTERNAL_CONFIGURATION_URL"
-      value = ""
+      valueFrom = data.aws_secretsmanager_secret.pem111_itk_external_configuration_url.arn
     }
-  ])
+  ]
 }
