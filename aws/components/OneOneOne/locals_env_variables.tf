a82215a4556b82d60782b1535edda19009071827
Chiara <chiara.mapelli@madetech.com>
2025-08-07 16:22:34 +0100

    Adding last secrets

--- aws/components/OneOneOne/locals_env_variables.tf
+++ aws/components/OneOneOne/locals_env_variables.tf
@@ -11,19 +11,19 @@ locals {
   ])
 }
 
-locals {
-  secret_variables = [
-    {
-      name      = "PEM111_AMQP_QUEUE_NAME"
-      valueFrom = data.aws_secretsmanager_secret.pem111_queue.arn
-    },
-    {
-      name = "PEM111_ITK_ODS_CODE_LIST"
-      valueFrom = data.aws_secretsmanager_secret.pem111_itk_ods_code_list.arn
-    },
-    {
-      name = "PEM111_ITK_EXTERNAL_CONFIGURATION_URL"
-      valueFrom = data.aws_secretsmanager_secret.pem111_itk_external_configuration_url.arn
-    }
-  ]
-}
+#locals {
+#  secret_variables = [
+#    {
+#      name      = "PEM111_AMQP_QUEUE_NAME"
+#      valueFrom = data.aws_secretsmanager_secret.pem111_queue.arn
+#    },
+#    {
+#      name = "PEM111_ITK_ODS_CODE_LIST"
+#      valueFrom = data.aws_secretsmanager_secret.pem111_itk_ods_code_list.arn
+#    },
+#    {
+#      name = "PEM111_ITK_EXTERNAL_CONFIGURATION_URL"
+#      valueFrom = data.aws_secretsmanager_secret.pem111_itk_external_configuration_url.arn
+#    }
+#  ]
+#}
--- aws/components/OneOneOne/locals_secret_variables.tf
+++ aws/components/OneOneOne/locals_secret_variables.tf
@@ -8,5 +8,17 @@ locals {
       name = "PEM111_AMQP_PASSWORD"
       valueFrom = data.aws_secretsmanager_secret.mq_password.arn
     },
+      {
+        name      = "PEM111_AMQP_QUEUE_NAME"
+        valueFrom = data.aws_secretsmanager_secret.pem111_queue.arn
+      },
+      {
+        name = "PEM111_ITK_ODS_CODE_LIST"
+        valueFrom = data.aws_secretsmanager_secret.pem111_itk_ods_code_list.arn
+      },
+      {
+        name = "PEM111_ITK_EXTERNAL_CONFIGURATION_URL"
+        valueFrom = data.aws_secretsmanager_secret.pem111_itk_external_configuration_url.arn
+      }
   ]
 }
