86ebee76eb372450a8417f6d2994b9f95c0f9f72
ORybak5 <12736698+ORybak5@users.noreply.github.com>
2025-11-26 13:34:14 +0000

    cluster param corrections

--- aws/components/base/docdb_cluster.tf
+++ aws/components/base/docdb_cluster.tf
@@ -20,10 +20,10 @@ resource "aws_docdb_cluster" "base_db_cluster" {
 resource "aws_docdb_cluster" "base_db_cluster_ver_5_0" {
   cluster_identifier = "${replace(local.resource_prefix,"_","-")}-dbcluster-ver-5-0"
   engine                          = "docdb"
+  engine_version                  = "4.0"
   backup_retention_period         = var.docdb_retention_period
   skip_final_snapshot             = true
   db_subnet_group_name            = aws_docdb_subnet_group.base_db_subnet_group.name
-  db_cluster_parameter_group_name = aws_docdb_cluster_parameter_group.base_db_parameters.name
   vpc_security_group_ids          = [ aws_security_group.docdb_sg.id ]
   enabled_cloudwatch_logs_exports = ["audit"]
   storage_encrypted               = var.docdb_storage_encrypted
